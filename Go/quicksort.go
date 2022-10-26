1a4794df4908dde737cf2ac46fa3971517e4018b
Glen Small <14293295+glensmall@users.noreply.github.com>
2022-10-26 18:37:41 +0100

    go implementation completed

--- Go/quicksort.go
+++ Go/quicksort.go
@@ -6,22 +6,43 @@ func main() {
 
 	fmt.Println("QuickSort Implementation in Go - Glen Small: October 2022")
 
-	var start int
-	var end int
-	unsortedData := [10]int{4, 7, 3, 5, 8, 1, 9, 2, 6, 0}
+	var start int = 0
+	var end int = 9
+	unsortedData := [10]int{44, 27, 3, 75, 88, 11, 9, 12, 36, 60}
+
+	fmt.Println()
+	fmt.Println("Unsorted array is:")
+	fmt.Println(unsortedData)
+
+	quickSort(&unsortedData, start, end)
+
+	fmt.Println("Sorted array is:")
+	fmt.Println(unsortedData)
 
 }
 
 // resursive function to do the sort
-func quickSortRecursive(_data *[10]int, _start int, _end int) {
+func quickSort(_data *[10]int, _start int, _end int) {
+
+	// process if start is less than end
+	if _start < _end {
+
+		var pivot = partition(_data, _start, _end)
+
+		// before the pivot
+		quickSort(_data, _start, pivot-1)
+
+		// after the pivot
+		quickSort(_data, pivot+1, _end)
+	}
 
 }
 
 // function to partition the array
-func partitionArray(_data *[10]int, _start int, _end int) int {
+func partition(_data *[10]int, _start int, _end int) int {
 
 	// position the pivot point
-	var pivot int = _data[_end]
+	var pivot int = (*_data)[_end]
 
 	// set the index of the smaller position
 	var index = _start - 1
@@ -30,17 +51,18 @@ func partitionArray(_data *[10]int, _start int, _end int) int {
 	for loop := _start; loop < _end; loop++ {
 
 		// swap if pivot is greater than data
-		if _data[loop] < pivot {
+		if (*_data)[loop] < pivot {
 			index++
-			var temp int = _data[index]
-			_data[index] = _data[loop]
-			_data[loop] = temp
+			var temp int = (*_data)[index]
+			(*_data)[index] = (*_data)[loop]
+			(*_data)[loop] = temp
 		}
 	}
 
-	var temp int = _data[index+1]
-	_data[index+1] = _data[_end]
-	_data[_end] = temp
+	// Swap the index with the end
+	var temp int = (*_data)[index+1]
+	(*_data)[index+1] = (*_data)[_end]
+	(*_data)[_end] = temp
 
 	return (index + 1)
 }
